71304678babb4b03dfd34c094cef65c0250e94f1
work-devps <work.devps@gmail.com>
2025-09-10 09:07:18 +0530

    Update serializers.py

--- subscription/serializers.py
+++ subscription/serializers.py
@@ -9,66 +9,6 @@ class FeatureSerializer(serializers.ModelSerializer):
         model = Feature
         fields = ['id', 'name']
 
-class PlanSerializer(serializers.ModelSerializer):
-    features = FeatureSerializer(many=True, read_only=True)
-    feature_ids = serializers.ListField(
-        child=serializers.IntegerField(), write_only=True, required=False
-    )
-    
-    class Meta:
-        model = Plan
-        fields = ['id', 'name', 'features', 'feature_ids']
-    
-    def create(self, validated_data):
-        feature_ids = validated_data.pop('feature_ids', [])
-        plan = Plan.objects.create(**validated_data)
-        if feature_ids:
-            plan.features.set(feature_ids)
-        return plan
-
-class SubscriptionListSerializer(serializers.ModelSerializer):
-    plan = PlanSerializer(read_only=True)
-    
-    class Meta:
-        model = Subscription
-        fields = ['id', 'start_date', 'is_active', 'plan']
-
-class SubscriptionCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscription
-        fields = ['plan']
-    
-    def create(self, validated_data):
-        validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
-
-class SubscriptionUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscription
-        fields = ['plan']
-    
-    def update(self, instance, validated_data):
-        # Deactivate current subscription
-        instance.is_active = False
-        instance.save()
-        
-        # Create new subscription with new plan
-        new_subscription = Subscription.objects.create(
-            user=instance.user,
-            plan=validated_data['plan'],
-            is_active=True
-        )
-        return new_subscription
-
-class SubscriptionDeactivateSerializer(serializers.Serializer):
-    """Serializer for subscription deactivation - no fields needed"""
-    pass
-
-class FeatureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Feature
-        fields = ['id', 'name']
-
 class PlanCreateSerializer(serializers.ModelSerializer):
     feature_ids = serializers.ListField(child=serializers.IntegerField())
     
@@ -123,7 +63,10 @@ class SubscriptionUpdateSerializer(serializers.ModelSerializer):
             is_active=True
         )
         return new_subscription
-    
+
+class SubscriptionDeactivateSerializer(serializers.Serializer):
+    """Serializer for subscription deactivation - no fields needed"""
+    pass
 
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
